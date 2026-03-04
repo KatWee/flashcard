1fea6cae460572d092314054f269023b56ed932c
KatWee <katkat.w@hotmail.com>
2026-03-04 18:48:47 +1100

    Add icon

--- flashcard-react/src/components/Card.js
+++ flashcard-react/src/components/Card.js
@@ -9,6 +9,8 @@ import React, { useState } from 'react';
 import PersonIcon from '@mui/icons-material/Person';
 import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
 import DeleteIcon from '@mui/icons-material/Delete';
+import VisibilitySharpIcon from '@mui/icons-material/VisibilitySharp';
+import VisibilityOffSharpIcon from '@mui/icons-material/VisibilityOffSharp';
 
 const bull = (
   <Box
@@ -19,8 +21,8 @@ const bull = (
 );
 
 export default function MyCard() {
-  const [showAnswer, setShowAnswer] = useState(false);
 
+  const [showAnswer, setShowAnswer] = useState(false);
   const toggleAnswer = () => setShowAnswer((s) => !s);
 
   return (
@@ -35,7 +37,8 @@ export default function MyCard() {
         </Typography>
         <Typography variant="body2">
           <Button onClick={toggleAnswer}>
-            {showAnswer ? 'Hide Answer' : 'Show Answer'}
+            {showAnswer ? <VisibilityOffSharpIcon /> : <VisibilitySharpIcon />}
+            {showAnswer ? ' Hide Answer' : ' Show Answer'}
         </Button>
             {showAnswer && <p>This is an answer</p>}
         </Typography>
